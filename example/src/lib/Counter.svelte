4421ae8c9d9a751e02d804abc90f3e5c8240e957
Rajaniraiyn R <72294760+Rajaniraiyn@users.noreply.github.com>
2023-08-16 23:52:48 +0530

    ⬆️ Bump v0.6.0

--- example/src/lib/Counter.svelte
+++ example/src/lib/Counter.svelte
@@ -1,13 +1,57 @@
 <script lang="ts">
-  import {sound} from "svelte-sound";
+  import { Sound, sound } from "svelte-sound";
   import click_mp3 from "../assets/click.mp3";
 
-  let count: number = 0
+  let count: number = 0;
   const increment = () => {
-    count += 1
+    count += 1;
+  };
+
+  const clickSound = new Sound(click_mp3); // this is reusable sound
+
+  function handleClick() {
+    clickSound.play();
+    setTimeout(() => {
+      clickSound.play();
+      count += 1; // this is to accompany the double click sound
+    }, 400);
   }
 </script>
 
-<button on:click={increment} use:sound={{src:click_mp3, events:["click"] }}>
-  count is {count}
-</button>
+<div>
+  <div class="button">
+    Action
+    <button
+      on:click={increment}
+      use:sound={{ src: click_mp3, events: ["click"] }}
+    >
+      count is {count}
+    </button>
+    <span> (single click sound) </span>
+  </div>
+
+  <div class="button">
+    Manual Sound
+    <button on:click={increment} on:click={handleClick}>
+      count is {count}
+    </button>
+    <span> (double click sound) </span>
+  </div>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+  .button {
+    display: inline-flex;
+    flex-direction: column;
+  }
+
+  span {
+    font-size: 0.8em;
+    color: #999;
+  }
+</style>
